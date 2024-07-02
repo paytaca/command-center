d8e50314cd0a02aea8638a6d33fb02e7f4381eaa
kqyannn <mrgabiana@up.edu.ph>
2024-07-02 10:23:47 +0800

    chore: Added cards for 'new merchant', placeholder, and 'vm status'

--- src/pages/AnalyticsPage.vue
+++ src/pages/AnalyticsPage.vue
@@ -3,22 +3,26 @@
     <div class="row q-col-gutter-md">
       <div class="col-md-3 col-sm-12 col-xs-12">
         <q-card style="height: 200px; background: linear-gradient(#07ffb8, #037454);">
-            <!-- <q-img src="~assets/bch_logo.png" style="width: 100px; height: 100px; margin: 0 auto;">
-            </q-img> -->
+            <q-img  src="~assets/bch_logo.png" class="bch" />
           <q-card-section class="align-center">
             <q-toolbar-title class="text-h6 text-bold text-primary">BCH Value</q-toolbar-title>
             <q-separator />
             <div class="column">
-              <h3 class="col q-ma-none">
+              <h3 class="col q-ma-none q-mt-md">
                 <a v-show="bchValue != 'Loading...'">{{ currencySymbols[selectedCurrency.value] }}</a> {{ bchValue.toLocaleString() }}
               </h3>
-              <q-select
-                v-model="selectedCurrency" dense
-                input-debounce="0" label="Select currency"
-                :options="currencyOptions" @input="fetchBCHValue"
-                behavior="menu" class="row justify-end"
-                style="width: 120px;"
-              />
+              <div class="row">
+                <q-select
+                  v-model="selectedCurrency" dense
+                  input-debounce="0" label="Select currency"
+                  :options="currencyOptions" @input="fetchBCHValue"
+                  behavior="menu" class="row justify-end q-mt-sm"
+                  style="width: 120px;"
+                />
+                <p>+++</p>
+                <p>---</p>
+              </div>
+
             </div>
           </q-card-section>
         </q-card>
@@ -42,30 +46,50 @@
         </q-card>
       </div>
 
-      <div class="col-md-4 col-md-4 col-sm-12 col-xs-12">
+      <div class="col-lg-4 col-md-4 col-sm-12 col-xs-12">
         <TransactionStats />
       </div>
-      <div class="col-md-4 col-md-4 col-sm-12 col-xs-12">
+      <div class="col-lg-4 col-md-4 col-sm-12 col-xs-12">
         <TransactionStats />
       </div>
-      <div class="col-md-4 col-md-4 col-sm-12 col-xs-12">
+      <div class="col-lg-4 col-md-4 col-sm-12 col-xs-12">
         <TransactionStats />
       </div>
+
+      <div class="col-lg-4 col-md-4 col-sm-12 col-xs-12">
+        <q-card style="height: 235px; background: linear-gradient(#5e6e94, #334155);">
+          <q-card-section class="align-center">
+            <q-toolbar-title class="text-h6 text-bold text-white">Recent Onboard Merchant</q-toolbar-title>
+            <q-separator color="white"/>
+
+          </q-card-section>
+        </q-card>
+      </div>
+      <div class="col-lg-5 col-md-4 col-sm-12 col-xs-12">
+        <q-card style="height: 235px; background: linear-gradient(#5e6e94, #334155);">
+          <q-card-section class="align-center">
+            <q-toolbar-title class="text-h6 text-bold text-white">Social Media Statistics</q-toolbar-title>
+            <q-separator color="white"/>
+
+          </q-card-section>
+        </q-card>
+      </div>
+      <div class="col-lg-3 col-md-4 col-sm-12 col-xs-12">
+        <q-card style="height: 235px; background: linear-gradient(#5e6e94, #334155);">
+          <q-card-section class="align-center">
+            <q-toolbar-title class="text-h6 text-bold text-white">Vending Machine Status</q-toolbar-title>
+            <q-separator color="white"/>
+
+          </q-card-section>
+        </q-card>
+      </div>
+
     </div>
   </q-page>
 </template>
 
-<style>
-/* Add this at the end of your <style> section */
-@media (min-width: 768px) { /* Adjust this breakpoint as needed */
-  .analytics-page {
-    min-height: 100vh; /* Make the page fill the viewport height */
-  }
-}
-</style>
-
 <script setup>
-import { ref, onMounted, defineAsyncComponent, watch } from 'vue'
+import { ref, onMounted, defineAsyncComponent } from 'vue'
 import axios from 'axios'
 
 // Async components
@@ -113,7 +137,24 @@ const fetchBCHValue = async () => {
 onMounted(() => {
   fetchBCHValue()
 })
-
-// Watcher for selectedCurrency
-watch(selectedCurrency, fetchBCHValue)
 </script>
+
+<style>
+.bch {
+  width: 130px;
+  height: 130px;
+  margin: 10px;
+  position: absolute;
+  overflow: auto;
+  bottom: 0;
+  right: 0;
+  max-width: 100%;
+  opacity: 0.5;
+}
+
+/* @media (min-width: 768px) {
+  .analytics-page {
+    min-height: 100vh;
+  }
+} */
+</style>
