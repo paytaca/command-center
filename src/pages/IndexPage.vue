536b3910c573b62d71994ac74345cb5ec9ceb083
kqyannn <mrgabiana@up.edu.ph>
2024-07-05 17:46:27 +0800

    refactor: Add comments to IndexPage.vue

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -1,5 +1,7 @@
 <template>
   <q-page style="background: linear-gradient(#1e293b,#44566e);">
+
+    <!-- Waves Design -->
     <div class="waves">
       <svg class="waves__svg" preserveAspectRatio="none" shape-rendering="auto" viewBox="0 24 150 28"
           xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
@@ -15,6 +17,7 @@
       </svg>
     </div>
 
+    <!-- Title Section -->
     <div class="column justify-center items-center content-center">
       <q-img src="../assets/title.png" style="max-height: 40vh; max-width: 50%;" class="row q-mt-xl"/>
       <p style="font-size: 3vw;" class="text-white q-ma-none self-end">Company Dashboard</p>
@@ -22,35 +25,30 @@
 
     <q-separator class="q-ma-lg bg-white"/>
 
+    <!-- Navigation Section -->
     <p style="font-size: 15px; opacity: 50%;" class="row justify-start content-start q-ma-none text-white q-ml-xl">Navigation</p>
     <div class="q-ma-lg">
       <div class="row q-col-gutter-x-xl q-col-gutter-y-lg justify-center">
+
+        <!-- Analytics button -->
         <div class="col-lg-2 col-md-3 col-sm-5 col-xs-10">
           <q-btn label="Analytics" icon="bar_chart" no-wrap stack dense class="fit text-white bg-secondary" @click="$router.push({ name: 'Analytics' })"/>
         </div>
+
+        <!-- Locations button -->
         <div class="col-lg-2 col-md-3 col-sm-5 col-xs-10">
           <q-btn label="Locations" icon="map" no-wrap stack dense class="fit text-white bg-secondary" @click="$router.push({ name: 'Locations' })"/>
         </div>
-        <!-- <div class="col-lg-2 col-md-3 col-sm-5 col-xs-10">
-          <q-btn label="Vending Machines" icon="bar_chart" no-wrap stack dense class="fit text-white bg-secondary" @click="$router.push({ name: 'Vending Machines' })"/>
-        </div> -->
+
       </div>
     </div>
-
-      <!-- <div dense class="row justify-center items-center text-center text-bold q-mx-md">
-        <div class="col">
-          <q-btn label="Analytics" icon="bar_chart" no-wrap stack dense class="text-white bg-secondary q-mt-none q-ma-sm" padding="1vh 3vh" @click="$router.push({ name: 'Analytics' })"/>
-          <q-btn label="Merchants" icon="storefront" no-wrap stack dense class="text-white bg-secondary q-mt-none q-ma-sm" padding="1vh 3vh" @click="$router.push({ name: 'Home' })"/>
-          <q-btn label="Vending Machines" icon="coffee_maker" no-wrap stack dense class="text-white bg-secondary q-mt-none q-ma-sm" padding="1vh 3vh" @click="$router.push({ name: 'Home' })"/>
-        </div>
-      </div> -->
-
   </q-page>
 </template>
 
 <script setup>
 import { ref } from 'vue'
 
+// Waves animation speed control
 const waveSpeed = ref(10)
 
 defineOptions({
@@ -66,17 +64,14 @@ defineOptions({
   overflow: hidden;
   bottom: 0;
 }
-
 .waves__svg {
   position: absolute;
   width: 100%;
   height: 100%;
 }
-
 .parallax > use {
   animation: moveWave 10s cubic-bezier(0.55, 0.5, 0.45, 0.5) infinite;
 }
-
 @keyframes moveWave {
   0% {
     transform: translate3d(-90px, 0, 0);
@@ -85,5 +80,4 @@ defineOptions({
     transform: translate3d(85px, 0, 0);
   }
 }
-
 </style>
