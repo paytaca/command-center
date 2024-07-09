33b2c828adc29986dbfa66fa263c15b515542ce0
kqyannn <mrgabiana@up.edu.ph>
2024-07-09 16:43:04 +0800

    refactor: Update database credentials and password in settings.py

--- command_center_backend/command_center_backend/settings.py
+++ command_center_backend/command_center_backend/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -85,8 +86,8 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'paytaca-cc-db',
-        'USER': 'DB_USER',
-        'PASSWORD': 'DB_PASSWORD',
+        'USER': config('DB_PASSWORD'),
+        'PASSWORD': config('DB_PASSWORD'),
         'HOST': 'localhost',
         'PORT': '5432',
     }
