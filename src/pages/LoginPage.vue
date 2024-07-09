afa8ce767683927a3102338a20409998c2b28f3e
kqyannn <mrgabiana@up.edu.ph>
2024-07-09 10:28:13 +0800

    chore: Update npm dependencies

--- src/pages/LoginPage.vue
+++ src/pages/LoginPage.vue
@@ -107,8 +107,6 @@
             <p class="text-grey-6">Forgot password?</p>
           </q-card-section>
         </q-card>
-      </div>
-    </div>
 
       <!-- Sticky Copyright -->
       <q-page-sticky position="bottom-left" :offset="[0,0]" class="q-pa-xs bg-primary text-white text-caption">
@@ -119,132 +117,111 @@
   </q-layout>
 </template>
 
-<script>
-import { ref, defineComponent } from 'vue'
+<script setup>
+import { ref } from 'vue'
 import { useQuasar } from 'quasar'
 import { axiosInstance } from 'boot/axios'
 import { useStore } from 'vuex'
 import { useRouter, useRoute } from 'vue-router'
 
-export default defineComponent({
-  name: 'LoginPage',
-  setup () {
-    const router = useRouter()
-    const route = useRoute()
-    const $q = useQuasar()
-    const store = useStore()
-    const title = ref('Log in')
-    const email = ref('')
-    const username = ref('')
-    const password = ref('')
-    const repassword = ref('')
-    const register = ref(false)
-    const passwordFieldType = ref('password')
-    const btnLabel = ref('Log in')
-    const visibility = ref(false)
-    const visibilityIcon = ref('visibility')
+const router = useRouter()
+const route = useRoute()
+const $q = useQuasar()
+const store = useStore()
+const title = ref('Log in')
+const email = ref('')
+const username = ref('')
+const password = ref('')
+const repassword = ref('')
+const register = ref(false)
+const passwordFieldType = ref('password')
+const btnLabel = ref('Log in')
+const visibility = ref(false)
+const visibilityIcon = ref('visibility')
 
-    const required = (val) => {
-      return (val && val.length > 0) ? true : 'Required'
-    }
-    const diffPassword = (val) => {
-      const val2 = password.value
-      return (val && (val === val2)) ? true : 'Password does not match'
-    }
+const required = (val) => {
+  return (val && val.length > 0) ? true : 'Required'
+}
+const diffPassword = (val) => {
+  const val2 = password.value
+  return (val && (val === val2)) ? true : 'Password does not match'
+}
 
-    const short = (val) => {
-      return (val && val.length >= 8) ? true : 'Password is too short'
-    }
+const short = (val) => {
+  return (val && val.length >= 8) ? true : 'Password is too short'
+}
 
-    const isEmail = (val) => {
-      const emailPattern = /^(?=[a-zA-Z0-9@._%+-]{6,254}$)[a-zA-Z0-9._%+-]{1,64}@(?:[a-zA-Z0-9-]{1,63}\.){1,8}[a-zA-Z]{2,63}$/
-      return (emailPattern.test(val)) ? true : 'Invalid email'
-    }
+const isEmail = (val) => {
+  const emailPattern = /^(?=[a-zA-Z0-9@._%+-]{6,254}$)[a-zA-Z0-9._%+-]{1,64}@(?:[a-zA-Z0-9-]{1,63}\.){1,8}[a-zA-Z]{2,63}$/
+  return (emailPattern.test(val)) ? true : 'Invalid email'
+}
 
-    const switchTypeForm = () => {
-      register.value = !register.value
-      title.value = register.value ? 'Register' : 'Log in'
-      btnLabel.value = register.value ? 'Register' : 'Log in'
-    }
+const switchTypeForm = () => {
+  register.value = !register.value
+  title.value = register.value ? 'Register' : 'Log in'
+  btnLabel.value = register.value ? 'Register' : 'Log in'
+}
 
-    const switchVisibility = () => {
-      visibility.value = !visibility.value
-      passwordFieldType.value = visibility.value ? 'text' : 'password'
-      visibilityIcon.value = visibility.value ? 'visibility_off' : 'visibility'
-    }
+const switchVisibility = () => {
+  visibility.value = !visibility.value
+  passwordFieldType.value = visibility.value ? 'text' : 'password'
+  visibilityIcon.value = visibility.value ? 'visibility_off' : 'visibility'
+}
 
-    const submitForm = async () => {
-      console.log('submitted')
-      if (register.value) {
-        const formData = {
-          email: email.value,
-          username: username.value,
-          password: password.value
-        }
-        console.log(formData)
-        try {
-          await axiosInstance.post('/api/v1/users/', formData)
-          $q.notify({
-            message: 'Account was created, please log in',
-            color: 'positive',
-            icon: 'info',
-            position: 'bottom-right',
-            timeout: 2000
-          })
-          switchTypeForm() // Switch to login after successful registration
-        } catch (error) {
-          console.error('Registration error:', error)
-          $q.notify({
-            message: 'There was a problem with your submission. Please check your data and try again.',
-            color: 'negative',
-            icon: 'error',
-            position: 'bottom-right',
-            timeout: 5000
-          })
-        }
-      } else {
-        // Login
-        const loginData = {
-          username: username.value,
-          password: password.value
-        }
-        try {
-          await store.dispatch('auth/doLogin', loginData)
-          const toPath = route.query.to || '/'
-          router.push(toPath)
-        } catch (err) {
-          console.error('Login error:', err)
-          $q.notify({
-            color: 'negative',
-            icon: 'error',
-            position: 'bottom-right',
-            timeout: 5000,
-            message: 'Login failed. Please check your credentials and try again.'
-          })
-        }
-      }
+const submitForm = async () => {
+  console.log('submitted')
+  if (register.value) {
+    const formData = {
+      email: email.value,
+      username: username.value,
+      password: password.value
     }
-
-    return {
-      title,
-      email,
-      username,
-      password,
-      repassword,
-      register,
-      passwordFieldType,
-      btnLabel,
-      visibility,
-      visibilityIcon,
-      required,
-      diffPassword,
-      short,
-      isEmail,
-      submitForm,
-      switchTypeForm,
-      switchVisibility
+    console.log(formData)
+    try {
+      await axiosInstance.post('/api/v1/users/', formData)
+      $q.notify({
+        message: 'Account was created, please log in',
+        color: 'positive',
+        icon: 'info',
+        position: 'bottom-right',
+        timeout: 2000
+      })
+      switchTypeForm() // Switch to login after successful registration
+    } catch (error) {
+      console.error('Registration error:', error)
+      $q.notify({
+        message: 'There was a problem with your submission. Please check your data and try again.',
+        color: 'negative',
+        icon: 'error',
+        position: 'bottom-right',
+        timeout: 5000
+      })
+    }
+  } else {
+    // Login
+    const loginData = {
+      username: username.value,
+      password: password.value
+    }
+    try {
+      await store.dispatch('auth/doLogin', loginData)
+      const toPath = route.query.to || '/'
+      router.push(toPath)
+    } catch (err) {
+      console.error('Login error:', err)
+      $q.notify({
+        color: 'negative',
+        icon: 'error',
+        position: 'bottom-right',
+        timeout: 5000,
+        message: 'Login failed. Please check your credentials and try again.'
+      })
     }
   }
+}
+
+defineOptions({
+  name: 'LoginPage'
 })
 </script>
 
