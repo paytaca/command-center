5450774ba9d1b0a46e843d9eee33cfdfa478fc00
aaflores7 <aaflores7@up.edu.ph>
2024-07-16 11:51:49 +0800

    refactor: Order locations, merchants, categories, and logos by ID in API views

--- command_center_backend/paytaca_map/views.py
+++ command_center_backend/paytaca_map/views.py
@@ -6,24 +6,24 @@ from .serializers import MerchantsSerializer, LocationSerializer, CategorySerial
 
 class LocationListAPIView(APIView):
     def get(self, request):
-        locations = Location.objects.all()
+        locations = Location.objects.all().order_by('id')
         serializer = LocationSerializer(locations, many=True)
         return Response(serializer.data)
 
 class MerchantListAPIView(APIView):
     def get(self, request):
-        merchants = Merchant.objects.filter(test_shop=False)
+        merchants = Merchant.objects.filter(test_shop=False).order_by('id')
         serializer = MerchantsSerializer(merchants, many=True)
         return Response(serializer.data)
     
 class CategoryListAPIView(APIView):
     def get(self, request):
-        categories = Category.objects.all()
+        categories = Category.objects.all().order_by('id')
         serializer = CategorySerializer(categories, many=True)
         return Response(serializer.data)
 
 class LogoListAPIView(APIView):
     def get(self, request):
-        logos = Logo.objects.all()
+        logos = Logo.objects.all().order_by('id')
         serializer = LogoSerializer(logos, many=True)
         return Response(serializer.data)
