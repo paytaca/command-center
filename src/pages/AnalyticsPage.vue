4962bf45771b9dd19989daaabff7827793da90af
Euvan Dave Siervo <58839850+essiervo@users.noreply.github.com>
2024-07-25 14:32:15 +0800

    refactor: Update wallet creation data scripts and CSV files

--- src/pages/AnalyticsPage.vue
+++ src/pages/AnalyticsPage.vue
@@ -39,7 +39,7 @@
               <div class="col-12 q-pb-sm q-mt-lg">
                 <q-card class="q-pa-sm row justify-between items-center"
                 style="background: linear-gradient(#07ffb8, #4871b8);">
-                  <a class="text-h6">{{ latestTransaction ? 'Transaction #' + latestTransaction.id : 'Loading...' }}</a>
+                  <a class="text-h6">{{ latestTransaction ? 'Transaction #' + totalCount : 'Loading...' }}</a>
                   <a>{{ latestTransaction ? latestTransaction.value * Math.pow(10, -latestTransaction.decimals) + ' ' +(latestTransaction.token.includes('bch') ? 'BCH' : 'BCH (CT)') : 'Loading...' }}</a>
                 </q-card>
               </div>
@@ -158,8 +158,7 @@
             <q-separator color="white"/>
 
             <div class="row justify-center items-center q-mt-xl text-white text-h6">
-              <q-icon name="construction_" />
-              {{ latestMerchant ? latestMerchant.name : 'Under construction...' }}
+              {{ latestMerchant ? latestMerchant.name : 'Loading...' }}
             </div>
 
           </q-card-section>
