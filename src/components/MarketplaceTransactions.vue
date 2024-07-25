82607b3e1072f4e3b20a903ab01dc5f240818c9e
Melben Kian Gabiana <gabianakian06@gmail.com>
2024-07-25 16:16:57 +0800

    refactor: Update MarketplaceTransactions.vue to improve table functionality and styling

--- src/components/MarketplaceTransactions.vue
+++ src/components/MarketplaceTransactions.vue
@@ -1,9 +1,11 @@
 <template>
   <q-table
     bordered title="Marketplace Transactions" row-key="name"
-    :rows="rows" :columns="columns" class="q-mb-md custom-scrollbar"
+    :rows="filteredRows" :columns="columns" class="custom-scrollbar"
     :visible-columns="visibleColumns"
-    style="max-height: 913px"
+    :rows-per-page-options="[10, 20, 30]"
+    style="max-height: 913px;" no-data-label="No data available"
+    separator="cell"
   >
     <template v-slot:top-right>
       <q-select
@@ -13,6 +15,7 @@
         emit-value option-value="name"
         :options="columns.filter(column => column.name !== 'txid')"
       />
+      <!-- Search Filter -->
       <q-input dense debounce="300" v-model="filter" placeholder="Search" class="q-ml-md">
         <template v-slot:append>
           <q-icon name="search" />
@@ -24,8 +27,12 @@
 </template>
 
 <script setup>
-import { ref } from 'vue'
+import { ref, onMounted, computed } from 'vue'
 
+// Visible columns variable
+const visibleColumns = ref(['txid', 'recipient', 'token', 'decimals', 'value', 'received_at'])
+
+// Columns data structure
 const columns = [
   {
     name: 'txid',
@@ -34,122 +41,41 @@ const columns = [
     align: 'left',
     field: row => row.txid,
     format: val => `${val}`,
-    sortable: true
+    sortable: true,
+    classes: 'col-3'
   },
-  { name: 'recipient', align: 'left', label: 'Recipient', field: 'recipient' },
-  { name: 'token', align: 'left', label: 'Token', field: 'token', sortable: true },
-  { name: 'decimals', align: 'left', label: 'Decimals', field: 'decimals', sortable: true },
-  { name: 'value', align: 'left', label: 'Value', field: 'value', sortable: true },
-  { name: 'date', align: 'left', label: 'Recieved at', field: 'date', sortable: true }
-  // { name: 'calcium', label: 'Calcium (%)', field: 'calcium', sortable: true, sort: (a, b) => parseInt(a, 10) - parseInt(b, 10) },
-  // { name: 'iron', label: 'Iron (%)', field: 'iron', sortable: true, sort: (a, b) => parseInt(a, 10) - parseInt(b, 10) }
+  { name: 'recipient', align: 'left', label: 'Recipient', field: 'recipient', sortable: true },
+  { name: 'token', align: 'left', label: 'Token', field: 'token', format: val => val.includes('ct') ? 'BCH (CT)' : 'BCH', sortable: true },
+  // { name: 'decimals', align: 'left', label: 'Decimals', field: 'decimals', sortable: true },
+  { name: 'value', align: 'left', label: 'Revenue', field: row => row.value * Math.pow(10, -row.decimals), sortable: true },
+  { name: 'received_at', align: 'left', label: 'Date', field: 'received_at', sortable: true, format: val => new Date(val).toLocaleString() }
 ]
 
-const rows = [
-  {
-    txid: 's250asz9cc0dsasdadsadsadsadsadsa',
-    recipient: 159,
-    token: 6.0,
-    decimals: 24,
-    value: 4.0,
-    date: 87,
-    calcium: '14%',
-    iron: '1%'
-  },
-  {
-    txid: 'Ice cream sandwich',
-    recipient: 237,
-    token: 9.0,
-    decimals: 37,
-    value: 4.3,
-    date: 129,
-    calcium: '8%',
-    iron: '1%'
-  },
-  {
-    txid: 'Eclair',
-    recipient: 262,
-    token: 16.0,
-    decimals: 23,
-    value: 6.0,
-    date: 337,
-    calcium: '6%',
-    iron: '7%'
-  },
-  {
-    txid: 'Cupcake',
-    recipient: 305,
-    token: 3.7,
-    decimals: 67,
-    value: 4.3,
-    date: 413,
-    calcium: '3%',
-    iron: '8%'
-  },
-  {
-    txid: 'Gingerbread',
-    recipient: 356,
-    token: 16.0,
-    decimals: 49,
-    value: 3.9,
-    date: 327,
-    calcium: '7%',
-    iron: '16%'
-  },
-  {
-    txid: 'Jelly bean',
-    recipient: 375,
-    token: 0.0,
-    decimals: 94,
-    value: 0.0,
-    date: 50,
-    calcium: '0%',
-    iron: '0%'
-  },
-  {
-    txid: 'Lollipop',
-    recipient: 392,
-    token: 0.2,
-    decimals: 98,
-    value: 0,
-    date: 38,
-    calcium: '0%',
-    iron: '2%'
-  },
-  {
-    txid: 'Honeycomb',
-    recipient: 408,
-    token: 3.2,
-    decimals: 87,
-    value: 6.5,
-    date: 562,
-    calcium: '0%',
-    iron: '45%'
-  },
-  {
-    txid: 'Donut',
-    recipient: 452,
-    token: 25.0,
-    decimals: 51,
-    value: 4.9,
-    date: 326,
-    calcium: '2%',
-    iron: '22%'
-  },
-  {
-    txid: 'KitKat',
-    recipient: 518,
-    token: 26.0,
-    decimals: 65,
-    value: 7,
-    date: 54,
-    calcium: '12%',
-    iron: '6%'
-  }
-]
+// Rows data array
+const rows = ref([])
 
+// Filter variable and function
 const filter = ref('')
-const visibleColumns = ref(['txid', 'recipient', 'token', 'decimals', 'value', 'date'])
+const filteredRows = computed(() => {
+  if (!filter.value) {
+    return rows.value // Return all rows if there's no filter
+  }
+  return rows.value.filter(row => row.txid.toLowerCase().includes(filter.value.toLowerCase()))
+})
+
+onMounted(async () => {
+  try {
+    const response = await fetch('http://127.0.0.1:8000/api/transactions/?format=json') // Replace 'YOUR_JSON_URL_HERE' with your actual JSON URL
+    if (!response.ok) {
+      throw new Error('Network response was not ok')
+    }
+    const data = await response.json()
+    rows.value = data // Assign fetched data to rowsc
+    console.log(rows.value)
+  } catch (error) {
+    console.error('There was a problem fetching the rows data:', error)
+  }
+})
 </script>
 
 <style scoped>
