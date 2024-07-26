e00025f3c296b3d4b037c6b2c2583ab8dc82fcbe
aaflores7 <aaflores7@up.edu.ph>
2024-07-26 10:10:09 +0800

    refactor: Update REST_FRAMEWORK authentication classes in settings.py

--- command_center_backend/command_center_backend/settings.py
+++ command_center_backend/command_center_backend/settings.py
@@ -94,6 +94,13 @@ DATABASES = {
     }
 }
 
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.BasicAuthentication',
+        'rest_framework.authentication.SessionAuthentication',
+    ]
+}
+
 
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
